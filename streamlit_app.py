ebdc4d8bd9264c504912ae197de5357b895a0835
Daniel Wilczak <44122838+danielwilczak101@users.noreply.github.com>
2023-06-03 10:38:12 -0500

    Update streamlit_app.py
    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
-streamlit.title('Hello World!')
+streamlit.title('My Parents New Heathly Diner')
+
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
